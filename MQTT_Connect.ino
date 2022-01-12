37e5e0b9905c92c5ae360a0d728bffc3c8ec4780
Frederik Justesen <s194035@student.dtu.dk>
2022-01-12 13:37:17 +0100

    Connect to MQTT broker and NodeRed

---
+++ MQTT_Connect.ino
@@ -0,0 +1,96 @@
+
+//////////// Initiering ///////////
+
+//inkluderer nødvendige bibloteker
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <SPI.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+
+///////// CALLBACKFUNKTION ////////
+
+// Definerer callback funktionen der modtager beskeder fra mqtt
+// OBS: den her funktion kører hver gang MCU'en modtager en besked via mqtt
+void callback(char* byteArraytopic, byte* byteArrayPayload, unsigned int length) {
+
+  // Konverterer indkomne besked (topic) til en string:
+  pinMode(IOpin1, OUTPUT);
+  String topic;
+  topic = String(byteArraytopic);
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.println("] ");
+  // Konverterer den indkomne besked (payload) fra en array til en string:
+  // Topic == Temperaturmaaler, Topic == Kraftsensor
+  if (topic == "frederikju@gmail.com/34338") { // OBS: der subscribes til et topic nede i reconnect-funktionen. I det her tilfælde er der subscribed til "Test". Man kan subscribe til alle topics ved at bruge "#"
+    payload = ""; // Nulstil payload variablen så forloopet ikke appender til en allerede eksisterende payload
+    for (int i = 0; i < length; i++) {
+      payload += (char)byteArrayPayload[i];
+    }
+    Serial.println(payload);
+    if(payload == "window_open"){digitalWrite(IOpin1, HIGH);}
+    else if(payload == "window_close"){digitalWrite(IOpin1, LOW);}
+    
+    //client.publish("frederikju@gmail.com/34338", "Hello World"); // Publish besked fra MCU til et valgt topic. Husk at subscribe til topic'et i NodeRed.
+  }
+
+}
+
+///////// CALLBACK SLUT /////////
+
+//
+//
+//
+//
+//
+//
+
+/////// OPSÆTNING AF WIFI-FORBINDELSE  ///////////
+
+
+// Opretter forbindelse til WiFi
+void setup_wifi() {
+  // Forbinder til et WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+// Opretter forbindelse til mqtt server. Dette gentages ved manglende forbindelse til WiFi, server osv.
+void reconnect() {
+  // Fortsætter til forbindelsen er oprettet
+  while (!client.connected()) {
+    Serial.print("Forsøger at oprette MQTT forbindelse...");
+
+    if (client.connect("GroupNamexMCU", mqtt_user, mqtt_pass)) { // Forbinder til klient med mqtt bruger og password
+      Serial.println("connected");
+      // Derudover subsribes til topic "Test" hvor NodeMCU modtager payload beskeder fra
+      client.subscribe("frederikju@gmail.com/34338");
+      // Der kan subscribes til flere specifikke topics
+      //client.subscribe("Test1");
+      // Eller til samtlige topics ved at bruge '#' (Se Power Point fra d. 18. marts)
+      // client.subscribe("#");
+
+      // Hvis forbindelsen fejler køres loopet igen efter 5 sekunder indtil forbindelse er oprettet
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Venter 5 sekunder før den prøver igen
+      delay(5000);
+    }
+  }
+}
